4d3bbf9b197144b9025c24aecc89b8b7b005cc36
Dzomo the everest Yak <everbld@microsoft.com>
2020-05-12 05:23:08 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=69c32106a06420348c9fa5d818211a0675bfb6dc
+hashes[FStar]=bce4d46926c81a940a313ce10b1bc72fba65782c
 branches[FStar]=master
-hashes[hacl-star]=e808e1383c52a22bb880d68f2129c22c29453c54
-branches[hacl-star]=_taramana_ubuntu_focal
-hashes[kremlin]=4c31087dc5900786aaadc6e6ad25cf24c0ccde81
-branches[kremlin]=master
-hashes[mitls-fstar]=c8d121fb0c8340a250d602c3297079cca8b5076f
-branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[quackyducky]=fb169abca963834fe512d64aa490f78c1e6b9060
+hashes[hacl-star]=dd87607dda81d9366a834a111953b3bf1faa421d
+branches[hacl-star]=_taramana_ubuntu_focal
+hashes[kremlin]=adea9dfe3f369379cf177982e38c66970f1120da
+branches[kremlin]=master
+hashes[mitls-fstar]=8bfbc99c754a54749f50f043fa6cded5938dbb72
+branches[mitls-fstar]=master
+hashes[quackyducky]=682ecec41c1fe98d8a051bfca13418523ef765a5
 branches[quackyducky]=master
