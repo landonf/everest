9b9f4fa52a1935e4ce343b8d7c66460277968fbe
Tahina Ramananandro <taramana@microsoft.com>
2020-06-24 18:02:30 -0700

    use F* _c_layeff, EverParse and miTLS _taramana_layeff

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=22f15bdb636c05df4d475571b444229efdf9c8bc
-branches[FStar]=master
+hashes[FStar]=73d5dae57f79ba8e0b55ce3fce3967d1a433030e
+branches[FStar]=_c_layeff
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[hacl-star]=32ba189487f3b08d9e8e0a29c92307f3444f7e74
 branches[hacl-star]=master
 hashes[kremlin]=0f08f1243331ffc62ba3f1b8d8b06e4de8479943
 branches[kremlin]=master
-hashes[mitls-fstar]=a1d38c4384052f82a46f3062b429a2b38655488c
-branches[mitls-fstar]=master
-hashes[quackyducky]=b2e464d4838e74edcb96921637becd494e2a6b9f
-branches[quackyducky]=master
+hashes[mitls-fstar]=ac2323499b851e3d71bc5ce05e20afa585085c5d
+branches[mitls-fstar]=_taramana_layeff
+hashes[quackyducky]=6e15cfaa3789633889303d6373b936df947c868d
+branches[quackyducky]=_taramana_layeff
